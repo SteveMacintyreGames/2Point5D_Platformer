7df2dbe81eb2fef989e040f5f1af3b7249420934
Steve Macintyre <SteveMacintyre868@gmail.com>
2021-06-03 17:36:51 -0700

    Completed Physics Based Character Controller

---
+++ Assets/Steves Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController _controller;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private float _jumpHeight = 15f;
+    private float _yVelocity; 
+    private bool _canDoubleJump;
+
+  
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput,0f,0f);
+        Vector3 velocity = direction * _speed;
+
+        if (_controller.isGrounded)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space) && _canDoubleJump == true)
+            {
+                _yVelocity += _jumpHeight;
+                _canDoubleJump = false;
+            }
+
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+
+    }
+}
